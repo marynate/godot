b941493dd73989c1ab435eac2f37729630f1c292
marynate <mary.w.nate@gmail.com>
2014-12-26 01:34:03 +0800

    Update Google MobileAd iOS SDK to 6.12.2 (.a file  not included)

--- modules/admob/config.py
+++ modules/admob/config.py
@@ -4,12 +4,15 @@ def can_build(plat):
 def configure(env):
 
 	if env['platform'] == "iphone":
-
-		env.Append(LINKFLAGS=['-ObjC','-framework','MessageUI','-framework','AdSupport','-framework','CoreTelephony','-framework','AVFoundation'])
+		
+		env.Append(LINKFLAGS=['-ObjC','-framework','AdSupport','-framework','AudioToolbox','-framework','AVFoundation','-framework','CoreGraphics','-framework','CoreTelephony','-framework','MessageUI','-framework','StoreKit','-framework','SystemConfiguration','-framework','EventKit','-framework','EventKitUI'])
 		env.Append(LIBPATH=['#modules/admob/sdk'])
 		env.Append(LIBS=['GoogleAdMobAds'])
+
 	if (env['platform'] == 'android'):
+		
 		#env.android_module_library("android/GoogleAdMobAdsSdk-6.4.1.jar")
 		env.android_module_file("android/GodotAdMob.java")
 		env.android_module_manifest("android/AndroidManifestChunk.xml")
 		env.disable_module()
+
